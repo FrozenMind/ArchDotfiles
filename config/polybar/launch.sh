95a12b6f5cefe04adf46f580b1780b087bbe2256
AUTHORNAME <AUTHOREMAIL>
2017-02-08 20:35:30 +0100

    my actual arch linux dotfiles. lets have fun with it

---
+++ config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Terminate already running instances of the bar
+killall -q polybar
+
+# Wait for the processes to shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+
+# Launch the bar
+polybar left -q &
+polybar right -q &
+
+echo "Launched the main bar ..."
