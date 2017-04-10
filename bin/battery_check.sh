c0ff322e9b905b8b3dda3a258472765cd4606613
FrozenMind <valentin.rebscher@live.de>
2017-04-10 17:14:51 +0200

    update: battery_check
    add: label for 20%, 15% and 10%
    add: every label will just displayed once, till u charged once

--- bin/battery_check.sh
+++ bin/battery_check.sh
@@ -1,8 +1,28 @@
+d20=0
+d15=0
+d10=0
 while true; do
-  BATTLIFE=$(acpi -b | awk '{print $4}' | cut -c1-2)
-  if [ "$BATTLIFE" -lt 20 ]
+  CORD=$(acpi -b | awk '{print $3}' | cut -c1)
+  if [ "$CORD" == "D" ]
     then
-      notify-send -u critical "BATTERY LOWER THAN 20%"
+      BATTLIFE=$(acpi -b | awk '{print $4}' | cut -c1-2)
+      if [ "$BATTLIFE" -lt 10 ] && [ "$d10" -eq 0 ]
+        then
+          notify-send -u critical "BATTERY LOWER THAN 10%"
+          d10=1
+        elif [ "$BATTLIFE" -lt 15 ] && [ "$d15" -eq 0 ]
+          then
+            notify-send -u critical "BATTERY LOWER THAN 15%"
+            d15=1
+        elif [ "$BATTLIFE" -lt 20 ] && [ "$d20" -eq 0 ]
+          then
+            notify-send -u normal "BATTERY LOWER THAN 20%"
+            d20=1
+      fi
+    else
+      d20=0
+      d15=0
+      d10=0
   fi
   sleep 60
 done
