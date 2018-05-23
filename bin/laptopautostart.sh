a00430e2163bfe6e156538b14ccf29351db34c50
Valentin Rebscher <val3nt1n.r3bsch3r.fm@gmail.com>
2018-05-23 10:20:09 +0200

    add week number to polybar & autostart

--- bin/laptopautostart.sh
+++ bin/laptopautostart.sh
@@ -1,3 +1,3 @@
-notify-send "Hello $USER" "Date: $(date +%d.%m.%Y)\nTime: $(date +%H:%M)"
+notify-send "Hello $USER" "Week: $(date +%V)\nDate: $(date +%d.%m.%Y)\nTime: $(date +%H:%M)"
 dropbox-cli start
 
