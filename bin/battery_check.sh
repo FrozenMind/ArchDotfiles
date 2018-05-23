71a8a1d9882cd36763106658eeacb32daa6f3568
Valentin Rebscher <val3nt1n.r3bsch3r.fm@gmail.com>
2018-05-23 10:19:08 +0200

    fix: battery-check does not show 100% battery as 10% anymore

--- bin/battery_check.sh
+++ bin/battery_check.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 STATUS=$(acpi -b | awk '{print $3}' | cut -c1)
-if [ "$STATUS" == "D" ]
+if [ "$STATUS" = "D" ]
   then
-    BATTLIFE=$(acpi -b | awk '{print $4}' | cut -c1-2)
-    if [ "$BATTLIFE" -lt 10 ]
+    BATTLIFE=$(acpi -b | awk '{print $4}' | cut -c1-3 | sed 's/\%//g')
+    if [ "$BATTLIFE" -lt 8 ]
       then
         notify-send -u critical "BATTERY LIFE: $BATTLIFE %"
-      elif [ "$BATTLIFE" -lt 15 ]
+      elif [ "$BATTLIFE" -lt 10 ]
         then
           notify-send -u normal "BATTERY LIFE: $BATTLIFE %"
-      elif [ "$BATTLIFE" -lt 20 ]
+      elif [ "$BATTLIFE" -lt 15 ]
         then
           notify-send -u low "BATTERY LIFE: $BATTLIFE %"
     fi
