b2773a83ee347a8afc79c5cedc35ea6710009527
Valentin Rebscher <val3nt1n.r3bsch3r.fm@gmail.com>
2018-05-23 10:21:36 +0200

    mirror hdmi was not fullhd

--- bin/mirror_hdmi.sh
+++ bin/mirror_hdmi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xrandr --fb 1920x1080 --output eDP-1 --mode 1600x900 --scale-from 1920x1080 --output HDMI-2 --mode 1920x1080 --scale 1x1 --same-as eDP-1
+xrandr --fb 1920x1080 --output eDP-1 --mode 1920x1080 --scale-from 1920x1080 --output HDMI-2 --mode 1920x1080 --scale 1x1 --same-as eDP-1
